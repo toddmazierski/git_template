35d0bc1bbe404eb959c594d11138d3886d510ccd
Todd Mazierski <toddmazierski@gmail.com>
2012-09-20 11:49:06 -0400

    Adding warning for long subjects, now wrapping body at 72 characters.

--- hooks/commit-msgs/word-wrap.rb
+++ hooks/commit-msgs/word-wrap.rb
@@ -9,5 +9,17 @@ def word_wrap(text, line_width = 80)
 end
 
 commit_message_pathname = Pathname.new(ARGV.first)
-wrapped_commit_message = word_wrap(commit_message_pathname.read)
+commit_message = commit_message_pathname.read
+
+# GitHub wraps subject lines longer than 69 characters.
+subject_line_width = 69
+subject_line = commit_message.lines.first.chomp
+if subject_line.length > subject_line_width
+  puts "warning: subject line of commit message > #{subject_line_width} characters"
+end
+
+# To see where the number 72 comes from, please see:
+#   http://tbaggery.com/2008/04/19/a-note-about-git-commit-messages.html
+line_width = 72
+wrapped_commit_message = word_wrap(commit_message, line_width)
 commit_message_pathname.open("w") { |file| file.write(wrapped_commit_message) }
