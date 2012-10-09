f85aa27b53cf91ea740e9bc4eb2ff656cd79be54
Todd Mazierski <toddmazierski@gmail.com>
2012-10-09 13:31:49 -0400

    Improving merge conflict regular exp. to reduce false positives.

--- hooks/pre-commits/merge-conflicts.rb
+++ hooks/pre-commits/merge-conflicts.rb
@@ -2,7 +2,7 @@ require File.expand_path("../../lib/commit_file", __FILE__)
 
 CommitFile.all.each do |commit_file|
   commit_file.lines.each do |line|
-    if line =~ /<<<<<<<|=======|>>>>>>>/
+    if line =~ /^<<<<<<<\s\w*|^=======$|^>>>>>>>\s\w*/
       line.alert("merge conflict characters detected")
     end
   end
