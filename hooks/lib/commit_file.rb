5bf98f2f47ba86f8a103b82eda4272defb800432
Todd Mazierski <toddmazierski@gmail.com>
2013-02-20 11:39:31 -0500

    Adding an extension blacklist.

--- hooks/lib/commit_file.rb
+++ hooks/lib/commit_file.rb
@@ -2,17 +2,21 @@ require "pathname"
 require File.expand_path("../line", __FILE__)
 
 class CommitFile
-  class << self
-    def all
-      pathnames.map { |pathname| new(pathname) }
-    end
+  EXTENSION_BLACKLIST = %w(.png .jpg .gif)
 
-    def pathnames
-      pathnames = ARGV.map { |argv| Pathname.new(argv) }
-      # Return only pathnames for files that exist.
-      pathnames.select { |pathname| pathname.exist? }
-    end
-  end # self
+  def self.all
+    pathnames.map { |pathname| new(pathname) }
+  end
+
+  def self.pathnames
+    pathnames = ARGV.map { |argv| Pathname.new(argv) }
+    # Return only pathnames for files that exist and aren't on the blacklist.
+    pathnames.select { |pathname| pathname.exist? and not blacklisted?(pathname) }
+  end
+
+  def self.blacklisted?(pathname)
+    EXTENSION_BLACKLIST.include?(pathname.extname)
+  end
 
   def initialize(pathname)
     @pathname = pathname
@@ -23,4 +27,4 @@ class CommitFile
       Line.new(@pathname, line, index)
     end
   end
-end # CommitFile
+end
